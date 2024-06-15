96d9e8986253eb29ba2374c3f3d7837956abb4fd
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-06-15 16:59:12 +0300

    Refactor parseAndValidate fun to a companion object

--- Parser.kt
+++ Parser.kt
@@ -1,28 +1,30 @@
 class Parser {
-    fun parseAndValidate(content: String): String {
-        val validChars = "<>+-[].,"
-        val filteredChars = StringBuilder(content.length)
-        val loopStack = mutableListOf<Int>()
+    companion object {
+        fun parseAndValidate(content: String): String {
+            val validChars = "<>+-[].,"
+            val filteredChars = StringBuilder(content.length)
+            val loopStack = mutableListOf<Int>()
 
-        for (charIndex in content.indices) {
-            val char = content[charIndex]
-            if (char in validChars) {
-                filteredChars.append(char)
-                if (char == '[') {
-                    loopStack.add(charIndex)
-                } else if (char == ']') {
-                    if (loopStack.isEmpty()) {
-                        throw IllegalStateException("Unmatched ']' at index $charIndex")
+            for (charIndex in content.indices) {
+                val char = content[charIndex]
+                if (char in validChars) {
+                    filteredChars.append(char)
+                    if (char == '[') {
+                        loopStack.add(charIndex)
+                    } else if (char == ']') {
+                        if (loopStack.isEmpty()) {
+                            throw IllegalStateException("Unmatched ']' at index $charIndex")
+                        }
+                        loopStack.removeAt(loopStack.size - 1)
                     }
-                    loopStack.removeAt(loopStack.size - 1)
                 }
             }
-        }
 
-        if (loopStack.isNotEmpty()) {
-            throw IllegalStateException("Unmatched '[' at index ${loopStack[0]}")
-        }
+            if (loopStack.isNotEmpty()) {
+                throw IllegalStateException("Unmatched '[' at index ${loopStack[0]}")
+            }
 
-        return filteredChars.toString()
+            return filteredChars.toString()
+        }
     }
 }
